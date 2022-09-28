c6eeaf2bb83449664fcfc9f4ec447602c5b5bde7
duexcoast <conor.ux@gmail.com>
2022-09-28 10:56:19 -0400

    added interceptor to remove findUser response object

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,35 +1,37 @@
-  import {
-    AfterInsert,
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    AfterUpdate,
-    AfterRemove,
-  } from 'typeorm';
+import {
+  AfterInsert,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterUpdate,
+  AfterRemove,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
 
-  @Entity()
-  export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  @Exclude()
+  password: string;
 
-    @AfterInsert()
-    logInsert() {
-      console.log('Inserted User with id', this.id);
-    }
-
-    @AfterUpdate()
-    logUpdate() {
-      console.log('Updated User with id', this.id);
-    }
-
-    @AfterRemove()
-    logRemove() {
-      console.log('Removed User with id', this.id);
-    }
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
   }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
+}
