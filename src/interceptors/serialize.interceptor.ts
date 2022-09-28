a363385df8ac97da29c873d91e9f42372332e2cb
duexcoast <conor.ux@gmail.com>
2022-09-28 12:56:25 -0400

    custom interceptor for transforming user entity into user dto

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(UserDto, data, {
+          excludeExtraneousValues: true
+        })
+      }),
+    );
+  }
+}
