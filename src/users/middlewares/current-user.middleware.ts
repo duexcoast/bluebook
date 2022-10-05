b2d5afe82a78f414a7cefc9fc1881ee0c034029e
duexcoast <conor.ux@gmail.com>
2022-10-05 11:36:03 -0400

    changed Request interface to allow currentUser

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,17 +1,29 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { User } from '../user.entity';
 import { UsersService } from '../users.service';
+
+// This is adding currentUser a property 
+// on the Express Request interface (optional)
+// and we're deifning the type as a User Entity instance
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
-  async use(req: Request, res: Response, next: NextFunction): boolean {
+  async use(req: Request, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      // @ts-ignore
+      // changed the Request interface to accept currentUser
       req.currentUser = user;
     }
 
